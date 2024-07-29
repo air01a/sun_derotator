58c11ce6e9a8726d0235363cabe463d319bed619
Erwan NIQUET <eniquet@damart.com>
2024-07-29 18:20:01 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# sun_derotator
-Derotate image when imaging sun with mosaics
+# SUN Image Derotator
+
+When you image the sun, you take a lot of pictures to take the best and stack them. It could take some times.
+When your telescope has a too large focale, the sun will not fit in one picture. So you take part and assemble them after.
+But, the whole process takes time, and if you have an altaz mount, the SUN picture will slowly rotate, and it could be hard to assemble afterward.
+This script make a rotation to compensate this apparent movment.
+
+AND Yes, I broke the event loop as I did edit images in the main loop, but not a problem for this kind of GUI :)
+
+The process for SUN Imaging :
+
+- Take SER files with 2K-3K images,
+- Assemble 100 images with Autostakkert,
+- Use Astrosurface to apply wavelet transformation, save as bmp
+- Use this script to rotate the pictures,
+- Use imerge to make the mosaics
+- Edit with gimp/photoshop/whatever you're used to use
+
+Enjoy the result :
+
+![image GUI](./images/gui.png)
+
+![image SUN](./images/2024.07.28_Soleil_PDS130.png)
---
+++ main.py
@@ -0,0 +1,103 @@
+import numpy as np
+from astropy.time import Time
+from astropy.coordinates import EarthLocation, AltAz, get_sun
+from astropy import units as u
+from PIL import Image
+import os
+from datetime import datetime
+import PySimpleGUI as sg
+import configparser
+
+
+height = 0  # Altitude de l'observateur en mètres
+config_file = 'config.ini'
+
+def read_config():
+    config = configparser.ConfigParser()
+    if os.path.exists(config_file):
+        config.read(config_file)
+    else:
+        config['DEFAULT'] = {'Longitude': '3.130800', 'Latitude': '50.669399', 'Directory': '.'}
+    return config
+
+def write_config(config):
+    with open(config_file, 'w') as configfile:
+        config.write(configfile)
+
+
+def calculate_rotation(time_utc, location, lat, lon):
+    time = Time(time_utc)
+    time.location=location
+    sun = get_sun(time)
+    altaz = AltAz(obstime=time, location=location)
+    sun_altaz = sun.transform_to(altaz)
+    rotation = (np.cos(lat * u.deg)  * np.cos(float(sun_altaz.az.radian))) / np.cos(float(sun_altaz.alt.radian))
+    return rotation*180/np.pi
+
+def rotate_image(image_path, rotation_angle):
+    image = Image.open(image_path)
+    rotated_image = image.rotate(rotation_angle, resample=Image.BICUBIC, expand=True)
+    rotated_image.save(image_path.replace(".bmp", "_rotated.bmp"))
+
+def run(path, lat, lon, writelog):
+    location = EarthLocation(lat=lat * u.deg, lon=lon * u.deg, height=height * u.m)
+    first_rotation = -1
+    files_dates=[]
+    for file in os.listdir(path):
+        if file.endswith('.bmp'):
+            try:
+                date_hour_str = file.split('.')[0].split('_')[0]
+                date_hour = datetime.strptime(date_hour_str, '%Y-%m-%d-%H%M')
+                date_utc_str = date_hour.strftime('%Y-%m-%dT%H:%M:00')
+                files_dates.append((file, date_utc_str))
+            except ValueError:
+                writelog(f'Le fichier {file} ne correspond pas au format attendu.')
+
+    for file_path,time_utc in files_dates:
+        writelog("++++++++ Start",colors='white on green')
+        rotation_angle = calculate_rotation(time_utc, location, lat, lon)
+        if first_rotation==-1:
+            first_rotation=rotation_angle
+        rotation_angle=rotation_angle-first_rotation
+        rotate_image(path+'/'+file_path, rotation_angle/2)
+        writelog(f"Applied a rotation of {rotation_angle:.2f} degrees to {file_path}")
+
+    writelog("Rotation correction applied to all images.")
+
+
+config = read_config()
+lon = config['DEFAULT'].get('Longitude', '')
+lat = config['DEFAULT'].get('Latitude', '')
+default_directory = config['DEFAULT'].get('Directory', '')
+
+layout = [
+    [sg.Text('Longitude'), sg.InputText(lon, key='Longitude')],
+    [sg.Text('Latitude'), sg.InputText(lat, key='Latitude')],
+    [sg.Text('Directory'), sg.InputText(default_directory, key='Directory'), sg.FolderBrowse()],
+    [sg.MLine(size=(100,20), write_only=True, expand_x=True, expand_y=True,key='-LOG-', reroute_cprint=True)],
+    [sg.Button('Run'), sg.Button('Exit')]
+]
+
+window = sg.Window('Configuration', layout)
+
+while True:
+    event, values = window.read()
+    
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+    elif event == 'Run':
+        longitude = values['Longitude']
+        latitude = values['Latitude']
+        directory = values['Directory']
+        
+        config['DEFAULT']['Longitude'] = longitude
+        config['DEFAULT']['Latitude'] = latitude
+        config['DEFAULT']['Directory'] = directory
+        write_config(config)
+        
+        print(sg.cprint)
+        run(directory, float(latitude), float(longitude),sg.cprint)
+
+
+window.close()
+
